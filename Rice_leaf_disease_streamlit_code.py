5a43dda42d24cc21a3a5183d0e6a9a9e39446b8c
Kiran Raj <131955358+Kiranraj28@users.noreply.github.com>
2025-10-05 18:40:33 +0530

    Update Rice_leaf_disease_streamlit_code.py

--- Rice_leaf_disease_streamlit_code.py
+++ Rice_leaf_disease_streamlit_code.py
@@ -9,20 +9,10 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 
+
 @st.cache_resource
 def load_model():
-    base_model = tf.keras.applications.MobileNetV2(input_shape=(256,256,3), include_top=False, weights='imagenet')
-    base_model.trainable = False
-    model = tf.keras.Sequential([
-        base_model,
-        tf.keras.layers.GlobalAveragePooling2D(),
-        tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dropout(0.5),
-        tf.keras.layers.Dense(3, activation='softmax')
-    ])
-    saved_model = tf.keras.models.load_model("best_model.keras", compile=False, safe_mode=False)
-    model.set_weights(saved_model.get_weights())
-    return model
+    return tf.keras.models.load_model("best_model_fixed.keras")
 
 model = load_model()
 CLASS_NAMES = ['Bacterial leaf blight', 'Brown spot', 'Leaf smut'] # adjust names as per your classes
